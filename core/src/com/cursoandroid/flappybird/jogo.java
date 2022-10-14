54ab736e5da30d5850e01d38c8045f95fe28e08c
anajorge <anajessicadegj@gmail.com>
2022-10-14 16:09:48 -0300

    inserting image

--- core/src/com/cursoandroid/flappybird/jogo.java
+++ core/src/com/cursoandroid/flappybird/jogo.java
@@ -1,31 +1,63 @@
 package com.cursoandroid.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class jogo extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+import org.w3c.dom.Text;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+public class jogo extends ApplicationAdapter {
+    private int movimentoX = 0;
+    private int movimentoY = 0;
+    private float variacao = 0;
+    private SpriteBatch batch;
+    private Texture[] passaros;
+    private Texture fundo;
+
+    private float larguraDispositivo;
+    private float alturaDispositivo;
+
+    @Override
+    public void create() {
+        //Gdx.app.log("create", "jogo iniciado");
+        batch = new SpriteBatch();
+        passaros = new Texture[3];
+        passaros[0] = new Texture("passaro1.png");
+        passaros[1] = new Texture("passaro2.png");
+        passaros[2] = new Texture("passaro3.png");
+        fundo = new Texture("fundo.png");
+
+        larguraDispositivo = Gdx.graphics.getWidth();
+        alturaDispositivo = Gdx.graphics.getHeight();
+
+    }
+
+    @Override
+    public void render() {
+        batch.begin();
+
+        if(variacao > 2)
+            variacao = 0;
+
+        batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
+        batch.draw(passaros[(int)variacao], 30, alturaDispositivo / 2);
+
+        variacao += Gdx.graphics.getDeltaTime() * 10;
+
+        movimentoX++;
+        movimentoY++;
+        batch.end();
+
+
+        //contador++;
+        //Gdx.app.log("render", "jogo renderizado" + contador);
+    }
+
+    @Override
+    public void dispose() {
+        //Gdx.app.log("dispose", "descarte de conteudos");
+
+    }
 }
